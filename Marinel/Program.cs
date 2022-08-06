3e75fc05ed63cedada05ac40241e9c301f53f8b2
Elijah Aremu <elijah@ElijahAremuMBP.lan>
2022-08-07 00:15:15 +0100

    Workflow testing

--- Marinel/Program.cs
+++ Marinel/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.IO;
+using System.Reflection;
 
 namespace SchoolDraftWebsite
 {
@@ -32,7 +33,8 @@ namespace SchoolDraftWebsite
 
             bldr.SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json")
-                .AddEnvironmentVariables();
+                .AddEnvironmentVariables()
+                .AddUserSecrets(Assembly.GetExecutingAssembly(), true);
         }
     }
 }
